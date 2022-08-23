e9d656d7c760b734b0e662b24e84c621001c0a28
Sergei Kochergin <s.kochergin@outlook.com>
2022-08-23 16:26:39 +0300

    Task 10 added

---
+++ Seminar_02/Task_10/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+bool askUser = true;
+int secondDigit = 0;
+
+while (askUser == true)
+{
+    System.Console.Write("Введите трехзначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number > 0)
+    {
+        if (number / 100 < 1 | number / 1000 >= 1)
+        {
+            System.Console.WriteLine("Вы ввели недопустимое число.");
+        }
+        else
+        {
+            secondDigit = number / 10 % 10;
+            System.Console.WriteLine($"Вторая цифра числа '{number}' -> {secondDigit}");
+            break;
+        }
+    }
+    
+
+    if (number < 0)
+    {
+        if (number / -100 < 1 | number / -1000 >= 1)
+        {
+            System.Console.WriteLine("Вы ввели недопустимое число.");
+        }
+        else
+        {
+            secondDigit = number / 10 % 10;
+            System.Console.WriteLine($"Вторая цифра числа '{number}' -> {secondDigit}");
+            break;
+        }
+    }
+}
+
+
