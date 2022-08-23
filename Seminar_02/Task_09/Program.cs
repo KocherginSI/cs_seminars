60cf31f497868a89f7e65514fb70a06def15246b
Sergei Kochergin <s.kochergin@outlook.com>
2022-08-23 23:52:50 +0300

    Task 09 added

---
+++ Seminar_02/Task_09/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello World!");
+    }
+
+    static int MaxNumber()
+    {
+        int number = new Random() .Next(10, 1000);
+        int max = 0;
+    }
+}
+
+
+
+
+
+// int number = new Random() .Next(10, 99);
+// int firstDigit = number % 10;
+// int secondDigit = number / 10;
+
+// System.Console.WriteLine(firstDigit);
+// System.Console.WriteLine(secondDigit);
+// if (firstDigit > secondDigit)
+// {
+//     System.Console.WriteLine(firstDigit);
+// }
+// else
+// {
+//     System.Console.WriteLine($"Max: {secondDigit}");
+// }
--- Seminar_02/Task_10/Program.cs
+++ Seminar_02/Task_10/Program.cs
@@ -1,45 +1,12 @@
-﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿using System;
 
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
-
-bool askUser = true;
-int secondDigit = 0;
-
-while (askUser == true)
+namespace Task_10
 {
-    System.Console.Write("Введите трехзначное число: ");
-    int number = Convert.ToInt32(Console.ReadLine());
-
-    if (number > 0)
+    class Program
     {
-        if (number / 100 < 1 | number / 1000 >= 1)
+        static void Main(string[] args)
         {
-            System.Console.WriteLine("Вы ввели недопустимое число.");
-        }
-        else
-        {
-            secondDigit = number / 10 % 10;
-            System.Console.WriteLine($"Вторая цифра числа '{number}' -> {secondDigit}");
-            break;
-        }
-    }
-    
-
-    if (number < 0)
-    {
-        if (number / -100 < 1 | number / -1000 >= 1)
-        {
-            System.Console.WriteLine("Вы ввели недопустимое число.");
-        }
-        else
-        {
-            secondDigit = number / 10 % 10;
-            System.Console.WriteLine($"Вторая цифра числа '{number}' -> {secondDigit}");
-            break;
+            System.Console.WriteLine("Hello World!");
         }
     }
 }
-
-
