ab058047b299542721a907675cb6d2f0049fab20
KocherginSI <111387752+KocherginSI@users.noreply.github.com>
2022-08-23 22:19:38 +0300

    Update Program.cs

--- Seminar_01/Task_02/Program.cs
+++ Seminar_01/Task_02/Program.cs
@@ -7,8 +7,10 @@
 
 Console.Write("Введите первое число: "); 
 int first_number = Convert.ToInt32(Console.ReadLine());
+
 Console.Write("Введите второе число: "); 
 int second_number = Convert.ToInt32(Console.ReadLine());
+
 
 if (first_number > second_number)
 {
