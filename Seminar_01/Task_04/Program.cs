5103d325ad0399d75d89d55dc7cf39a93aca2b7a
KocherginSI <111387752+KocherginSI@users.noreply.github.com>
2022-08-23 22:24:19 +0300

    Update Program.cs

--- Seminar_01/Task_04/Program.cs
+++ Seminar_01/Task_04/Program.cs
@@ -6,11 +6,14 @@
 
 Console.Write("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
+
 Console.Write("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
+
 Console.Write("Введите третье число: ");
 int c = Convert.ToInt32(Console.ReadLine());
 int max = a;
+
 
 if (b > a)
 {
