e910023356d0978e7bb85e6821ba13d67fa6959c
Sergei Kochergin <s.kochergin@outlook.com>
2022-08-24 23:23:08 +0300

    Task 13 added

---
+++ Seminar_02/Task_13/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+            System.Console.WriteLine("645 -> 5");
+            System.Console.WriteLine("78 -> третьей цифры нет");
+            System.Console.WriteLine("32679 -> 6");
+            System.Console.WriteLine();
+            ThirdNumber();
+        }
+
+        static int ThirdNumber()
+        {
+            System.Console.Write("Введите число: ");
+            int number = Convert.ToInt32(Console.Read());
+            int[] array = {};
+            int thirdNumber = 0;
+            int lastDigit = 0;
+
+            while (number >= 10)
+            {
+                lastDigit = number % 10;
+                array = new int[lastDigit];
+                number -= lastDigit;
+
+            }
+            
+            Console.WriteLine($"Length: {array.Length}");
+            foreach (int item in array) Console.WriteLine(item);
+
+
+            if (array.Length < 2)
+            {
+                System.Console.WriteLine($"-> Третьей цифры нет.");
+            }
+            else
+            {
+                thirdNumber = array[1];
+                System.Console.WriteLine($"-> {thirdNumber}");
+            }
+
+            return thirdNumber;
+
+        }
+    }
+}
