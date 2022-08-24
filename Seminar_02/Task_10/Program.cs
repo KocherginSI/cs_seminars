0c78fe911c4c79902853a0c5a4e6ccf94210269b
Sergei Kochergin <s.kochergin@outlook.com>
2022-08-24 20:15:29 +0300

    Task 10 added

--- Seminar_02/Task_10/Program.cs
+++ Seminar_02/Task_10/Program.cs
@@ -6,7 +6,24 @@ namespace Task_10
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+            System.Console.WriteLine("456 -> 5");
+            System.Console.WriteLine("782 -> 8"); 
+            System.Console.WriteLine("918 -> 1");
+            System.Console.WriteLine();
+            SecondDigit();
         }
+    
+        static void SecondDigit()
+        {
+            int number = new Random() .Next(100, 1000);
+            int secondDigit = number;
+            secondDigit %= 100;
+            secondDigit /= 10; 
+
+            System.Console.WriteLine($"Случайное трехзначное число: {number}");
+            System.Console.WriteLine($"Вторая цифра числа: {secondDigit}");
+        }
+    
     }
 }
