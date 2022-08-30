90ca5ac719697346aa0d9684d615e75df363bd99
Sergei Kochergin <s.kochergin@outlook.com>
2022-08-30 22:34:40 +0300

    Task 25 added

---
+++ Seminar_04/Task_25/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Введите число A:");
+            int numberA = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Введите число B:");
+            int numberB = Convert.ToInt32(Console.ReadLine());
+
+            
+            double exponent = Exponent(numberA, numberB);
+            System.Console.WriteLine($"{numberA} ^ {numberB} -> {exponent}");
+            
+        }
+
+        static double Exponent(int numberA, int numberB)
+        {
+        
+            double result = numberA;
+            for (int i = 1; i < numberB; i++)
+            {   
+                result *= numberA;
+
+            }
+
+            return result;
+        }
+         
+    }
+}
