fe2dc512d67f67b0cc77b17a657d20b3a804e1b5
Sergei Kochergin <s.kochergin@outlook.com>
2022-08-30 16:00:05 +0300

    Task 21 modified

--- Seminar_03/Task_21/Program.cs
+++ Seminar_03/Task_21/Program.cs
@@ -6,22 +6,25 @@ namespace Task_21
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Введите координаты первой точки (x, y):");
+            System.Console.WriteLine("Введите координаты первой точки (x, y, z):");
             double firstX = Convert.ToDouble(Console.ReadLine());
             double firstY = Convert.ToDouble(Console.ReadLine());
-            System.Console.WriteLine("Введите координаты второй точки (x, y):");
+            double firstZ = Convert.ToDouble(Console.ReadLine());
+            System.Console.WriteLine("Введите координаты второй точки (x, y, z):");
             double secondX = Convert.ToDouble(Console.ReadLine());
             double secondY = Convert.ToDouble(Console.ReadLine());
+            double secondZ = Convert.ToDouble(Console.ReadLine());
 
-            Distance(firstX, firstY, secondX, secondY);            
+
+            Distance(firstX, firstY, firstZ, secondX, secondY, secondZ);
 
         }
 
 
-        static void Distance(double firstX, double firstY, double secondX, double secondY)
+        static void Distance(double firstX, double firstY, double firstZ, double secondX, double secondY, double secondZ)
         {
-            double distance = Math.Sqrt((Math.Pow(secondX - firstX, 2) + (Math.Pow(secondY - firstY, 2))));
-            System.Console.WriteLine($"distance: {distance}");
+            double distance = Math.Sqrt((Math.Pow(secondX - firstX, 2)) + (Math.Pow(secondY - firstY, 2)) + (Math.Pow(secondZ - firstZ, 2)));
+            System.Console.WriteLine($"distance: {distance:f2}");
         }
 
     }
