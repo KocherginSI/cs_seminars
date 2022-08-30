67e603f20edffa74d70d43cfdb5ec8d42fbce5cf
Sergei Kochergin <s.kochergin@outlook.com>
2022-08-30 16:20:22 +0300

    Task 23 added

---
+++ Seminar_03/Task_23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Введите число:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            CubicTable(number);
+
+
+        }
+
+        static void CubicTable(int number)
+        {
+            for (int i = 1;  i <= number; i++)
+            {
+                double result = Math.Pow(i, 3);
+                System.Console.Write($"{result} ");
+            }
+        }
+    }
+
+}
