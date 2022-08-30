350b2b6ea5829f1985ca69859d0387455f42c9d5
Sergei Kochergin <s.kochergin@outlook.com>
2022-08-30 22:49:24 +0300

    Task 27 added

---
+++ Seminar_04/Task_27/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Введите число:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int result = SummOfDigits(number);
+            System.Console.WriteLine($"Сумма цифр числа {number} равна: {result}");
+        
+
+        }
+
+        static int SummOfDigits(int number)
+        {
+            int summ = 0;
+            while (number != 0)
+            {
+                summ += number % 10;
+                number /= 10;
+            }
+            return summ;
+        }
+    }
+}
