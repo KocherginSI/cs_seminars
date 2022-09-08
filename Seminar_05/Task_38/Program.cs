27c85bdddc8d311c9d71eada24e2902093f0430a
Sergei Kochergin <s.kochergin@outlook.com>
2022-09-09 00:48:34 +0300

    Task 38 added

---
+++ Seminar_05/Task_38/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+using System;
+
+namespace Task_38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Введите количество элементов массива:");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            
+            RandomArrayDifference(userNumber);
+        }
+
+
+        static void RandomArrayDifference(int randomArrayLength)
+        {
+
+            int randomArrayIndex = 0;
+            double randomDifference = 0;
+            
+            double[] randomArray = new double[randomArrayLength];
+     
+            System.Console.WriteLine("Массив случайных вещественных чисел:");
+
+            while (randomArrayIndex < randomArrayLength)
+            {
+                randomArray[randomArrayIndex] = new Random() .NextDouble();
+                System.Console.Write($"{Math.Round(randomArray[randomArrayIndex], 4)} ");
+                randomArrayIndex++;
+
+            }  
+
+            Array.Sort(randomArray);
+
+            randomDifference = randomArray[randomArrayIndex - 1] - randomArray[0];
+            System.Console.WriteLine("\nРазница между максимальным и минимальным элементом массива:");
+            System.Console.WriteLine("{0:f4}", randomDifference);
+
+        
+
+        }
+
+    }
+
+}
